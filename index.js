05612ef384586b1832dcb932cf28c4c81a72d6f1
Ethan Yang <ethan.y163@gmail.com>
2021-11-29 21:39:54 +0800

    feat: freeze the idx

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const React = (function () {
-  let _val;
+  let hooks = [];
+  let idx = 0;
   function useState(initVal) {
-    const state = _val || initVal;
+    const state = hooks[idx] || initVal;
+    const _idx = idx;
     const setState = (newVal) => {
-      _val = newVal;
+      hooks[_idx] = newVal;
     };
+    idx++;
     return [state, setState];
   }
 
   function render(Component) {
+    idx = 0;
     const C = Component();
     C.render();
     return C;
@@ -19,19 +23,17 @@ const React = (function () {
 
 function Component() {
   const [count, setCount] = React.useState(1);
+  const [text, setText] = React.useState("apple");
 
   return {
-    render: () => console.log(count),
+    render: () => console.log({ count, text }),
     click: () => setCount(count + 1),
+    type: (word) => setText(word),
   };
 }
 
 var App = React.render(Component);
 App.click();
 var App = React.render(Component);
-App.click();
-var App = React.render(Component);
-App.click();
-var App = React.render(Component);
-App.click();
+App.type("pear");
 var App = React.render(Component);
